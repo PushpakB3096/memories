d580556bea85458f8ce41d71472d1e1cdcd7540f
PushpakB3096 <rtpushpak@gmail.com>
2021-04-03 15:11:27 +0530

    Update controller to fetch posts from the db

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,12 @@
-export const getPosts = (req, res) => {
-  res.send("TESTING");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 };
