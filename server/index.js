bfed9e3d0c8ffb387a7b7cd6936bd1ad75ff4894
PushpakB3096 <rtpushpak@gmail.com>
2021-04-03 11:51:21 +0530

    Setup mongo atlas and server

--- server/index.js
+++ server/index.js
@@ -3,3 +3,41 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+// constants go here
+const CONNECTION_URL =
+  "mongodb+srv://pushpak-memories:pass123@cluster0.keakc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+// only allow 30mb of file size for images
+app.use(
+  bodyParser.json({
+    limit: "30mb",
+    extended: true,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+
+// allow CORS
+app.use(cors());
+
+// connect to mongoose
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  })
+  .catch((error) => console.error(error.message));
+
+mongoose.set("useFindAndModify", false);
