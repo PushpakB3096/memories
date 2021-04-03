d74f62a913d670fdb239ef61055a1002217cd691
PushpakB3096 <rtpushpak@gmail.com>
2021-04-03 15:15:21 +0530

    Add controller to create new posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,26 @@
 import PostMessage from "../models/postMessage.js";
 
+// controller to fetch all the posts
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+// controller to create a new post
+export const createPost = async (req, res) => {
+  const newPost = req.body;
+  const createdPost = new PostMessage(newPost);
+  try {
+    await createdPost.save();
+    res.status(209).json(createdPost);
+  } catch (error) {
+    res.status(409).json({
       message: error.message,
     });
   }
