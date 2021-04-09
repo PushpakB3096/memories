be7644ab600647e3b4568d8143b2dc331a14f24a
PushpakB3096 <rtpushpak@gmail.com>
2021-04-10 01:44:39 +0530

    Add navbar phase 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Posts from "./components/Posts/Posts.js";
 import Form from "./components/Form/Form.js";
 import useStyles from "./styles";
-import memories from "./images/memories.png";
 // related to redux
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
+import Navbar from "./components/Navbar/Navbar.js";
 
 const App = () => {
   const classes = useStyles();
@@ -21,17 +22,7 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          height="60"
-          alt="memories"
-        />
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
           <Grid
