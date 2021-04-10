b3b852dcc49d89bf5634dcdc8896d89bd5a8c51c
PushpakB3096 <rtpushpak@gmail.com>
2021-04-10 14:05:08 +0530

    Add middleware for auth

---
+++ server/middleware/auth.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    /**
+     * we can have two types of tokens -
+     * 1. customer created by us
+     * 2. provided by Google auth
+     *
+     * The token provided by Google auth has the length greater than 500.
+     * That is not the case when the token is created by us.
+     */
+    const isCustomToken = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomToken) {
+      // decode the jwt using our own secret
+      decodedData = jwt.verify(token, process.env.SECRET);
+
+      req.userId = decodedData?.id;
+    } else {
+      // decode the jwt without a secret
+      decodedData = jwt.verify(token);
+
+      // 'sub' is the ID equivalent of google users
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default auth;
