f3c722114aadb053a56cc5b630f5594957f50682
PushpakB3096 <rtpushpak@gmail.com>
2021-04-10 11:21:10 +0530

    Fix dev URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // API is deployed on Heroku
-const BACKEND_URL = "https://pb-memories-api.herokuapp.com/posts";
+// const BACKEND_URL = "https://pb-memories-api.herokuapp.com/posts";
+const BACKEND_URL = "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(BACKEND_URL);
 export const createPost = (data) => axios.post(BACKEND_URL, data);
