0f326b5b140014e791556d8e9cec5e219c35cb16
PushpakB3096 <rtpushpak@gmail.com>
2021-04-10 11:20:55 +0530

    Fix console error

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -26,15 +26,17 @@ const Input = ({
         type={type}
         // inputProps only applicable for password field in order to show the password on click
         inputProps={
-          name === "password" && {
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={handleShowPassword}>
-                  {type === "password" ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password" ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
         }
       />
     </Grid>
