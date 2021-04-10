5afe6d6429fbd40cbabe24ca4cc764087bef5ac9
PushpakB3096 <rtpushpak@gmail.com>
2021-04-10 14:47:55 +0530

    Refactor API calls in the client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,11 +2,19 @@ import axios from "axios";
 
 // API is deployed on Heroku
 // const BACKEND_URL = "https://pb-memories-api.herokuapp.com/posts";
-const BACKEND_URL = "http://localhost:5000/posts";
+const BACKEND_URL = "http://localhost:8080";
 
-export const fetchPosts = () => axios.get(BACKEND_URL);
-export const createPost = (data) => axios.post(BACKEND_URL, data);
-export const updatePost = (id, data) =>
-  axios.patch(`${BACKEND_URL}/${id}`, data);
-export const deletePost = (id) => axios.delete(`${BACKEND_URL}/${id}`);
-export const likePost = (id) => axios.patch(`${BACKEND_URL}/${id}/likePost`);
+const API = axios.create({
+  baseURL: BACKEND_URL,
+});
+
+// post related
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (data) => API.post("/posts", data);
+export const updatePost = (id, data) => API.patch(`/posts/${id}`, data);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+// user auth related
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
