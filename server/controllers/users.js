e87741d4311e8342c56febd0444b6fbeead3a06d
PushpakB3096 <rtpushpak@gmail.com>
2021-04-10 13:56:41 +0530

    Add user sign up controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -37,7 +37,7 @@ export const signIn = async (req, res) => {
       { expiresIn: "1h" }
     );
 
-    res.status(200).json({
+    return res.status(200).json({
       result: existingUser,
       token,
     });
@@ -48,4 +48,54 @@ export const signIn = async (req, res) => {
   }
 };
 
-export const signUp = async (req, res) => {};
+export const signUp = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  try {
+    // gets at most one user because email is unique
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: `User with email: ${email} already exists`,
+      });
+    }
+
+    // check if both the passwords match or not
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        message: `Passwords don't match`,
+      });
+    }
+
+    // hash password with salt difficulty as 12
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    /* at this point, we have a new user created.
+        now we need to create and send a jwt
+    */
+    const token = jwt.sign(
+      {
+        email: result.email,
+        id: result._id,
+      },
+      process.env.SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({
+      result,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Something went wrong. ${error}`,
+    });
+  }
+};
