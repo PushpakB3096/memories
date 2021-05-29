f6680d835d54e591a7e315eea6d97932888faaec
PushpakB3096 <rtpushpak@gmail.com>
2021-05-29 15:27:04 +0530

    Add styling to form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,8 +7,8 @@ import { createPost, updatePost } from "../../actions/posts";
 
 const Form = ({ currentId, setCurrentId }) => {
   // if we have a current ID, then that means we are editing a post. In that case, we need to fetch the same post from the state
-  const post = useSelector((state) =>
-    currentId ? state.posts.find((p) => p._id === currentId) : null
+  const post = useSelector(state =>
+    currentId ? state.posts.find(p => p._id === currentId) : null
   );
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -24,11 +24,11 @@ const Form = ({ currentId, setCurrentId }) => {
     title: "",
     message: "",
     tags: "",
-    selectedFile: "",
+    selectedFile: ""
   });
 
   // function to handle the submit of this form
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     // to prevent browser refresh
     e.preventDefault();
 
@@ -37,7 +37,7 @@ const Form = ({ currentId, setCurrentId }) => {
       dispatch(
         updatePost(currentId, {
           ...postData,
-          name: user?.result?.name,
+          name: user?.result?.name
         })
       );
     } else {
@@ -45,7 +45,7 @@ const Form = ({ currentId, setCurrentId }) => {
         createPost({
           ...postData,
           // gets the name of the user from local storage and sends it as the creator of the post
-          name: user?.result?.name,
+          name: user?.result?.name
         })
       );
     }
@@ -59,7 +59,7 @@ const Form = ({ currentId, setCurrentId }) => {
       title: "",
       message: "",
       tags: "",
-      selectedFile: "",
+      selectedFile: ""
     });
     setCurrentId(null);
   };
@@ -68,7 +68,7 @@ const Form = ({ currentId, setCurrentId }) => {
   if (!user?.result?.name) {
     return (
       <Paper className={classes.paper}>
-        <Typography variant="h6" align="center">
+        <Typography variant='h6' align='center'>
           Please sign in to create your own memories
         </Typography>
       </Paper>
@@ -76,64 +76,64 @@ const Form = ({ currentId, setCurrentId }) => {
   }
 
   return (
-    <Paper className={classes.paper}>
+    <Paper className={classes.paper} elevation={6}>
       <form
         className={`${classes.form} ${classes.root}`}
-        autoComplete="off"
+        autoComplete='off'
         noValidate
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">
+        <Typography variant='h6'>
           {currentId ? "Editing" : "Creating"} a Memory
         </Typography>
         {/* input for title of the memory */}
         <TextField
-          name="title"
-          variant="outlined"
-          label="Title"
+          name='title'
+          variant='outlined'
+          label='Title'
           fullWidth
           value={postData.title}
-          onChange={(e) =>
+          onChange={e =>
             setPostData({
               ...postData,
-              title: e.target.value,
+              title: e.target.value
             })
           }
         />
         {/* input for a message */}
         <TextField
-          name="message"
-          variant="outlined"
-          label="Message"
+          name='message'
+          variant='outlined'
+          label='Message'
           fullWidth
           value={postData.message}
-          onChange={(e) =>
+          onChange={e =>
             setPostData({
               ...postData,
-              message: e.target.value,
+              message: e.target.value
             })
           }
         />
         {/* input for memory tags */}
         <TextField
-          name="tags"
-          variant="outlined"
-          label="Tags (coma separated)"
+          name='tags'
+          variant='outlined'
+          label='Tags (coma separated)'
           fullWidth
           value={postData.tags}
-          onChange={(e) =>
+          onChange={e =>
             setPostData({ ...postData, tags: e.target.value.split(",") })
           }
         />
         {/* for image upload */}
         <div className={classes.fileInput}>
           <FileBase
-            type="file"
+            type='file'
             multiple={false}
             onDone={({ base64 }) =>
               setPostData({
                 ...postData,
-                selectedFile: base64,
+                selectedFile: base64
               })
             }
           />
@@ -141,18 +141,18 @@ const Form = ({ currentId, setCurrentId }) => {
         {/* for submitting the form */}
         <Button
           className={classes.buttonSubmit}
-          variant="contained"
-          color="primary"
-          size="large"
-          type="submit"
+          variant='contained'
+          color='primary'
+          size='large'
+          type='submit'
           fullWidth
         >
           Submit
         </Button>
         <Button
-          variant="contained"
-          color="secondary"
-          size="small"
+          variant='contained'
+          color='secondary'
+          size='small'
           onClick={clear}
           fullWidth
         >
