249065624a51a919202891bfd7b9b2872c1eaa2e
PushpakB3096 <rtpushpak@gmail.com>
2021-06-22 10:06:18 +0530

    Add smoother transitions on home and post details page

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -3,7 +3,8 @@ import {
   Paper,
   Typography,
   CircularProgress,
-  Divider
+  Divider,
+  Grow
 } from "@material-ui/core/";
 import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
@@ -58,78 +59,83 @@ const Post = () => {
   return (
     <>
       {post && (
-        <Paper style={{ padding: "20px", borderRadius: "15px" }} elevation={6}>
-          <div className={classes.card}>
-            <div className={classes.section}>
-              <Typography variant='h4' component='h2'>
-                {post?.title}
-              </Typography>
-              <Typography
-                gutterBottom
-                variant='h6'
-                color='textSecondary'
-                component='h2'
-              >
-                {post?.tags?.map(tag => `#${tag} `)}
-              </Typography>
-              <Typography gutterBottom variant='body1' component='p'>
-                {post?.message}
-              </Typography>
-              <Typography variant='h6'>Created by: {post.name}</Typography>
-              <Typography variant='caption'>
-                {moment(post?.createdAt).fromNow()}
-              </Typography>
-              <Divider style={{ margin: "20px 0" }} />
-              <Typography variant='body1'>
-                <strong>Comments - Might be coming later</strong>
-              </Typography>
-              <Divider style={{ margin: "20px 0" }} />
-            </div>
-            <div className={classes.imageSection}>
-              <img
-                className={classes.media}
-                src={
-                  post?.selectedFile ||
-                  "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
-                }
-                alt={post?.title}
-              />
-            </div>
-          </div>
-          {/* Recommended posts sections goes here */}
-          {recommendedPosts.length > 0 && (
-            <div className={classes.section}>
-              <Typography variant='h5' gutterBottom>
-                You might also be interested in:
-              </Typography>
-              <Divider />
-              <div className={classes.recommendedPosts}>
-                {recommendedPosts.map(
-                  ({ title, message, name, likes, selectedFile, _id }) => (
-                    <div
-                      style={{ margin: "20px", cursor: "pointer" }}
-                      onClick={() => history.push(`/posts/${_id}`)}
-                    >
-                      <Typography variant='h6' gutterBottom>
-                        {title}
-                      </Typography>
-                      <Typography variant='subtitle2' gutterBottom>
-                        {name}
-                      </Typography>
-                      <Typography variant='subtitle2' gutterBottom>
-                        {message}
-                      </Typography>
-                      <Typography variant='subtitle1' gutterBottom>
-                        Likes: {likes.length > 0 ? likes.length : "0"}
-                      </Typography>
-                      <img src={selectedFile} width='210px' height='175px' />
-                    </div>
-                  )
-                )}
+        <Grow in>
+          <Paper
+            style={{ padding: "20px", borderRadius: "15px" }}
+            elevation={6}
+          >
+            <div className={classes.card}>
+              <div className={classes.section}>
+                <Typography variant='h4' component='h2'>
+                  {post?.title}
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant='h6'
+                  color='textSecondary'
+                  component='h2'
+                >
+                  {post?.tags?.map(tag => `#${tag} `)}
+                </Typography>
+                <Typography gutterBottom variant='body1' component='p'>
+                  {post?.message}
+                </Typography>
+                <Typography variant='h6'>Created by: {post.name}</Typography>
+                <Typography variant='caption'>
+                  {moment(post?.createdAt).fromNow()}
+                </Typography>
+                <Divider style={{ margin: "20px 0" }} />
+                <Typography variant='body1'>
+                  <strong>Comments - Might be coming later</strong>
+                </Typography>
+                <Divider style={{ margin: "20px 0" }} />
+              </div>
+              <div className={classes.imageSection}>
+                <img
+                  className={classes.media}
+                  src={
+                    post?.selectedFile ||
+                    "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+                  }
+                  alt={post?.title}
+                />
               </div>
             </div>
-          )}
-        </Paper>
+            {/* Recommended posts sections goes here */}
+            {recommendedPosts.length > 0 && (
+              <div className={classes.section}>
+                <Typography variant='h5' gutterBottom>
+                  You might also be interested in:
+                </Typography>
+                <Divider />
+                <div className={classes.recommendedPosts}>
+                  {recommendedPosts.map(
+                    ({ title, message, name, likes, selectedFile, _id }) => (
+                      <div
+                        style={{ margin: "20px", cursor: "pointer" }}
+                        onClick={() => history.push(`/posts/${_id}`)}
+                      >
+                        <Typography variant='h6' gutterBottom>
+                          {title}
+                        </Typography>
+                        <Typography variant='subtitle2' gutterBottom>
+                          {name}
+                        </Typography>
+                        <Typography variant='subtitle2' gutterBottom>
+                          {message}
+                        </Typography>
+                        <Typography variant='subtitle1' gutterBottom>
+                          Likes: {likes.length > 0 ? likes.length : "0"}
+                        </Typography>
+                        <img src={selectedFile} width='210px' height='175px' />
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
+            )}
+          </Paper>
+        </Grow>
       )}
     </>
   );
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, CircularProgress, Paper } from "@material-ui/core";
+import { Grid, CircularProgress, Paper, Grow } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
@@ -16,18 +16,20 @@ const Posts = ({ setCurrentId }) => {
       <CircularProgress size='7em' />
     </Paper>
   ) : (
-    <Grid
-      className={classes.container}
-      container
-      alignItems='stretch'
-      spacing={3}
-    >
-      {posts?.map(post => (
-        <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
-          <Post post={post} setCurrentId={setCurrentId} />
-        </Grid>
-      ))}
-    </Grid>
+    <Grow in>
+      <Grid
+        className={classes.container}
+        container
+        alignItems='stretch'
+        spacing={3}
+      >
+        {posts?.map(post => (
+          <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 };
 
